07b595caf64009973df2031b663f942ea6ff21c3
curoli <curoli@gmail.com>
2019-04-10 18:24:53 -0400

    Setting version within source.

--- src/main/scala/chowser/app/ChowserAppInfo.scala
+++ src/main/scala/chowser/app/ChowserAppInfo.scala
@@ -2,7 +2,7 @@ package chowser.app
 
 object ChowserAppInfo {
 
-  val version: String = "0.0.1"
+  val version: String = "1.1.0"
   val name: String = "chowser"
   val fullName: String = name + " " + version
 
