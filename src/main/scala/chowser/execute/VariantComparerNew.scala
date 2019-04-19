734a616b49c33fbfd4acb5978d2a2d89e6250a42
Oliver Ruebenacker <oliverr@broadinstitute.org>
2019-04-19 16:58:52 -0400

    Implementing compare variants command, part 6.

--- src/main/scala/chowser/execute/VariantComparerNew.scala
+++ src/main/scala/chowser/execute/VariantComparerNew.scala
@@ -1,7 +1,8 @@
 package chowser.execute
 
 import better.files.File
-import chowser.util.genomics.VariantIdLocation
+import chowser.util.genomics.{Location, VariantIdLocation}
+import chowser.util.genomics.Location.Ordering
 import chowser.util.genomics.VariantIdLocation.VariantIdLocationTsvWriter
 
 case class VariantComparerNew(idKey: String, chromosomeKey: String, positionKey: String) {
@@ -15,6 +16,22 @@ case class VariantComparerNew(idKey: String, chromosomeKey: String, positionKey:
     val inBothSink = Sink.forFileOpt(inBothFileOpt)
     val inOneOnlySink = Sink.forFileOpt(inOneOnlyFileOpt)
     val inTwoOnlySink = Sink.forFileOpt(inTwoOnlyFileOpt)
+    if(!iter1.hasNext) {
+      iter2.foreach(inTwoOnlySink.write)
+    } else if(!iter2.hasNext) {
+      iter1.foreach(inOneOnlySink.write)
+    } else {
+      var variants1: Set[VariantIdLocation] = Set.empty
+      var variants2: Set[VariantIdLocation] = Set.empty
+      val currentVariant1 = iter1.next()
+      val currentVariant2 = iter2.next()
+      val currentLocation = Set(currentVariant1.location, currentVariant2.location).min
+      val currentLocationVariants1: Set[VariantIdLocation] = Set.empty
+      val currentLocationVariants2: Set[VariantIdLocation] = Set.empty
+      val variantLaterLocationOpt1: Option[Location] = None
+      val variantLaterLocationOpt2: Option[Location] = None
+      val variantsAtCurrent1: Set[VariantIdLocation] = Set(currentVariant1)
+    }
     ???
   }
 
@@ -39,6 +56,11 @@ case class VariantComparerNew(idKey: String, chromosomeKey: String, positionKey:
 
   object NoOpSink extends Sink {
     override def write(variantIdLocation: VariantIdLocation): Unit = ()
+  }
+
+  case class VariantBuffer(variantsHere1: Set[VariantIdLocation], variantsHere2: Set[VariantIdLocation],
+                           variantAheadOpt1: Option[VariantIdLocation], variantAheadOpt2: Option[VariantIdLocation]) {
+
   }
 
 }
