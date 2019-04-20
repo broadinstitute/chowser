e83900a50a399edaa49afe9fd8af66fc212479b0
Oliver Ruebenacker <oliverr@broadinstitute.org>
2019-04-20 16:55:40 -0400

    Implementing compare variants command, part 7.

--- src/main/scala/chowser/execute/VariantComparerNew.scala
+++ src/main/scala/chowser/execute/VariantComparerNew.scala
@@ -16,9 +16,9 @@ case class VariantComparerNew(idKey: String, chromosomeKey: String, positionKey:
     val inBothSink = Sink.forFileOpt(inBothFileOpt)
     val inOneOnlySink = Sink.forFileOpt(inOneOnlyFileOpt)
     val inTwoOnlySink = Sink.forFileOpt(inTwoOnlyFileOpt)
-    if(!iter1.hasNext) {
+    if (!iter1.hasNext) {
       iter2.foreach(inTwoOnlySink.write)
-    } else if(!iter2.hasNext) {
+    } else if (!iter2.hasNext) {
       iter1.foreach(inOneOnlySink.write)
     } else {
       var variants1: Set[VariantIdLocation] = Set.empty
@@ -51,6 +51,7 @@ case class VariantComparerNew(idKey: String, chromosomeKey: String, positionKey:
 
   case class FileSink(file: File) extends Sink {
     val delegate = new VariantIdLocationTsvWriter(idKey, chromosomeKey, positionKey)(file)
+
     override def write(variantIdLocation: VariantIdLocation): Unit = delegate.add(variantIdLocation)
   }
 
@@ -58,9 +59,55 @@ case class VariantComparerNew(idKey: String, chromosomeKey: String, positionKey:
     override def write(variantIdLocation: VariantIdLocation): Unit = ()
   }
 
-  case class VariantBuffer(variantsHere1: Set[VariantIdLocation], variantsHere2: Set[VariantIdLocation],
-                           variantAheadOpt1: Option[VariantIdLocation], variantAheadOpt2: Option[VariantIdLocation]) {
+  class Channels(val iter1: Iterator[VariantIdLocation], val iter2: Iterator[VariantIdLocation],
+                 val inBothSink: Sink, val inOneOnlySink: Sink, val inTwoOnlySink: Sink)
 
+  trait VariantBuffer {
+    def hasLocation: Boolean = false
+
+    def variantsHere1: Set[VariantIdLocation]
+
+    def variantsHere2: Set[VariantIdLocation]
+
+    def variantAheadOpt1: Option[VariantIdLocation]
+
+    def variantAheadOpt2: Option[VariantIdLocation]
+
+    def channels: Channels
+
+    def isDoneWith1: Boolean = variantsHere1.isEmpty && variantAheadOpt1.isEmpty && channels.iter1.isEmpty
+
+    def isDoneWith2: Boolean = variantsHere2.isEmpty && variantAheadOpt2.isEmpty && channels.iter2.isEmpty
+  }
+
+
+  object VariantBuffer {
+    def apply(channels: Channels): VariantBufferFlushed = VariantBufferFlushed(channels)
+  }
+
+  case class VariantBufferFlushed(variantAheadOpt1: Option[VariantIdLocation],
+                                  variantAheadOpt2: Option[VariantIdLocation])(val channels: Channels)
+    extends VariantBuffer {
+    def advance: VariantBuffer = {
+      ??? // TODO
+    }
+
+    override def variantsHere1: Set[VariantIdLocation] = Set.empty
+
+    override def variantsHere2: Set[VariantIdLocation] = Set.empty
+  }
+
+  object VariantBufferFlushed {
+    def apply(channels: Channels): VariantBufferFlushed =
+      VariantBufferFlushed(None, None)(channels)
+  }
+
+  case class VariantBufferAtLocation(location: Location,
+                                     variantsHere1: Set[VariantIdLocation],
+                                     variantsHere2: Set[VariantIdLocation],
+                                     variantAheadOpt1: Option[VariantIdLocation],
+                                     variantAheadOpt2: Option[VariantIdLocation])(val channels: Channels)
+    extends VariantBuffer {
   }
 
 }
