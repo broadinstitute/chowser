2ce64bfdeb288833d083e171ef0a9d51e6a7689b
curoli <curoli@gmail.com>
2019-05-07 15:54:32 -0400

    Advance version from 1.4.0 to 1.5.0.

--- src/main/scala/chowser/app/ChowserAppInfo.scala
+++ src/main/scala/chowser/app/ChowserAppInfo.scala
@@ -2,7 +2,7 @@ package chowser.app
 
 object ChowserAppInfo {
 
-  val version: String = "1.4.0"
+  val version: String = "1.5.0"
   val name: String = "chowser"
   val fullName: String = name + " " + version
 
