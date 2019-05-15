ba38521313e408205b6d7b5d878c0b0d6b89df3e
Oliver Ruebenacker <oliverr@broadinstitute.org>
2019-05-15 15:24:40 -0400

    Adding tests for MathUtils.probit, part 2.

--- src/test/scala/chowser/util/MathUtilsTest.scala
+++ src/test/scala/chowser/util/MathUtilsTest.scala
@@ -13,8 +13,24 @@ class MathUtilsTest extends FunSuite {
   }
 
   test("probit") {
-    assertPrecise(MathUtils.probit(0.025), -1.959964)
+    val delta = 1e-15
+    assert(MathUtils.probit(-delta).isNaN)
+    assert(MathUtils.probit(0.0).isNaN)
+    assert(!MathUtils.probit(delta).isNaN)
+    assertPrecise(MathUtils.probit(0.5), 0.0)
+    assert(!MathUtils.probit(1.0 - delta).isNaN)
+    assert(MathUtils.probit(1.0).isNaN)
+    assert(MathUtils.probit(1.0 + delta).isNaN)
 
+    val x0 = delta
+    val x1 = 1.0 - delta
+    val n = 20
+    for(i <- 0 to 20) {
+      val q = i.toDouble/n
+      val x = (1.0 - q)*x0 + q*x1
+      println(x + "\t" + MathUtils.probit(x))
+    }
+    assertPrecise(MathUtils.probit(0.025), -1.959964)
   }
 
 }
