0fbd32102e0fa88ae185f50e6747d0060b322fec
curoli <curoli@gmail.com>
2019-05-24 16:01:43 -0400

    Advancing version from 1.6.1 to 1.6.2.

--- src/main/scala/chowser/app/ChowserAppInfo.scala
+++ src/main/scala/chowser/app/ChowserAppInfo.scala
@@ -2,7 +2,7 @@ package chowser.app
 
 object ChowserAppInfo {
 
-  val version: String = "1.6.1"
+  val version: String = "1.6.2"
   val name: String = "chowser"
   val fullName: String = name + " " + version
 
