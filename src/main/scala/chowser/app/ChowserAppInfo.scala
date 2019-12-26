45a129a6f16d0fbd835fc91d381864e4c5ce99c4
Oliver A Ruebenacker <oliverr@broadinstitute.org>
2019-12-26 15:10:58 -0500

    Advancing version from 1.7.3 to 1.7.4.

--- src/main/scala/chowser/app/ChowserAppInfo.scala
+++ src/main/scala/chowser/app/ChowserAppInfo.scala
@@ -2,7 +2,7 @@ package chowser.app
 
 object ChowserAppInfo {
 
-  val version: String = "1.7.3"
+  val version: String = "1.7.4"
   val name: String = "chowser"
   val fullName: String = name + " " + version
 
