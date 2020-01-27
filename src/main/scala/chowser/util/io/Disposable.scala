5dedcf1802d50fff9c66e04acfb5db458e7775d4
curoli <curoli@gmail.com>
2020-01-27 18:57:36 -0500

    Disposable class stub, part 1.

--- src/main/scala/chowser/util/io/Disposable.scala
+++ src/main/scala/chowser/util/io/Disposable.scala
@@ -1,6 +1,8 @@
 package chowser.util.io
 
-case class Disposable[A](a: A)(disposer: () => Unit) {
+import chowser.util.io.Disposable.Disposer
+
+case class Disposable[A](a: A)(private val disposer: Disposer) {
   private var isDisposedVar: Boolean = false
 
   def get: A = a
@@ -10,15 +12,37 @@ case class Disposable[A](a: A)(disposer: () => Unit) {
   def isDisposed: Boolean = isDisposedVar
 
   def dispose(): Unit = {
-    disposer()
+    disposer.dispose()
     isDisposedVar = true
   }
 
   def useUp[B](user: A => B): B = {
     val b = user(a)
-    disposer()
+    disposer.dispose()
     b
   }
 
   def map[B](mapper: A => B): Disposable[B] = Disposable[B](mapper(a))(disposer)
+
+  def map[B](mapper: A => Disposable[B]): Disposable[B] = ???
+}
+
+object Disposable {
+
+  trait Disposer {
+    def dispose(): Unit
+
+    def ++ : Disposer
+  }
+
+  object Disposer {
+
+    case class Composite(disposers: Seq[Disposer]) extends Disposer {
+      override def dispose(): Unit = disposers.foreach(_.dispose())
+
+      override def ++ : Disposer = ???
+    }
+
+  }
+
 }
